1878fdc0e7962ea44ec1231f5247d3094e9749f5
Message Akunna <message.ao@gmail.com>
2019-09-16 21:05:17 +0100

    design so far

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
-document.getElementById('showPassword')
-	.addEventListener("click", function(event){
-		var checkVal = document.getElementById("inputPassword");
-		if (checkVal.type === "password") {
-			checkVal.type = "text";
-		} else {
-			checkVal.type = "password";
-		}
-	});
+$(document).ready(function() {
+	$('#showPassword').on('click', function(event){
+			var checkVal = $('#inputPassword');
+			if (checkVal.attr('type') === 'password') {
+				checkVal.attr('type', 'text');
+			} else {
+				checkVal.attr('type', 'password');
+			}
+		});
+	$('form').on("submit", function(e){
+			e.preventDefault();
+			$('#submitBtn')
+				.attr('disabled', true)
+					.html('<i class="fas fa-spinner fa-pulse fa-fw "></i> Processing');
+			var formData = new FormData(this);
+			$.ajax({
+				url: 'auth/validate.php',
+				type : 'POST',
+	            data : formData,
+	            processData: false,
+	            cache: false,
+	            contentType: false,
+	            success: function(data){
+	            	$('#submitBtn')
+	            		.attr('disabled', false)
+	            			.html('<i class="fas fa-sign-in-alt" id="signIn"></i> Sign in');
+	            	if (data){
+	            		/* user this data to log in
+	            			email: message.ao@gmail.com
+	            			password: 1234;
+	            		*/
+	            		alert('you have successfully logged in.')
+						// console.log(data);
+	            	}
+	            	else{
+	            		$('.alert').removeClass('d-none');
+	            	}
+	            }
+			})
+		});
+});
+
